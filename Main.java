5c9b7db6f501a09a40049c6acf1cad8583951fc5
MahiaVP <sunnix035@gmail.com>
2026-05-12 12:28:16 -0500

    Crear archivo de texto

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         TextFileWriter writer = new TextFileWriter("ejemplo.txt");
+        System.out.println("Programa para crear archivo sobre tu personaje favorito");
+        System.out.println("Como se llama tu personaje?");
+        String name = sc.nextLine();
+        System.out.println("De que serie/pelicula/videojuego/etc es tu personaje?");
+        String place = sc.nextLine();
+        System.out.println("Cuando es el cumpleaños de tu personaje?");
+        String bd = sc.nextLine();
+        System.out.println("Cual es la caracteristica principal de tu personaje?");
+        String car = sc.nextLine();
+        System.out.println("Cual es la debilidad de tu personaje?");
+        String deb = sc.nextLine();
+
 
         try {
             writer.openFile();
-            writer.writeToFile("Hola, mundo!\n");
-            writer.writeToFile("Este es un archivo de texto plano creado en Java y POO");
+            writer.writeToFile("Nombre: "+name+" - "+place+"\n");
+            writer.writeToFile("Cumpleaños: "+bd+"\n");
+            writer.writeToFile("Caracteristica principal: "+car+"\n");
+            writer.writeToFile("Debilidad: "+deb+"\n");
             System.out.println("Archivo creado exitosamente");
         } catch (IOException e){
             System.err.println("Error al escribir en el archivo: "+e.getMessage());
@@ -15,7 +31,7 @@ public class Main {
             try{
                 writer.closeFile();
             }catch (IOException e){
-                System.err.println("Error al errar el archivo: "+e.getMessage());
+                System.err.println("Error al cerrar el archivo: "+e.getMessage());
             }
         }
     }
